dfd38dfed71ca111dd44092cfe5dfcfb8f588d02
Alexandre Muraviov <amuraviov@globalia.ca>
2024-01-31 05:44:48 -0500

    WIP: query to retrieve the messages

--- app/Console/Commands/PublishMessages.php
+++ app/Console/Commands/PublishMessages.php
@@ -35,7 +35,11 @@ class PublishMessages extends Command
         // $this->info(sprintf('Pushed to `%s` queue: [%s] `%s` ', $queue, $message->messagable->type, $message->messagable->title));
 
         $now = Carbon::now();
-        Message::whereBetween('publish_at', $now->startOfMinute(), $now->endOfMinute())
+        Message::whereBetween(
+                'published_at', 
+                $now->startOfMinute()->toDateTimeString(), 
+                $now->endOfMinute()->toDateTimeString()
+            )
             ->where('status', 0)
             ->each(function (Message $message) use ($queue) {
                 dispatch(new PublishMessage($message))->onQueue($queue);       
