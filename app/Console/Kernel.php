a80e235e7dcae875f90e60a33b6329a4b8e37a25
Alexandre Muraviov <amuraviov@globalia.ca>
2024-02-08 11:42:12 -0500

    unpublish messages eveery minute

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use App\Console\Commands\PublishMessages;
+use App\Console\Commands\UnpublishMessages;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -14,6 +15,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command(PublishMessages::class)->everyMinute();
+        $schedule->command(UnpublishMessages::class)->everyMinute();
     }
 
     /**
